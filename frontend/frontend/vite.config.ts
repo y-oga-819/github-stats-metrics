a4691d6213662458e014963f8aa3d7591e31d6da
yuki819 <yuki819@mba2018.local>
2023-11-26 23:58:48 +0900

    feat: backendのコンテナと通信できるようにする

--- frontend/frontend/vite.config.ts
+++ frontend/frontend/vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT || '3000'),
+    proxy:{
+      '/api': {
+        target: process.env.API_URL, // docker-compose.yamlで指定したURLに対してリクエストを投げさせる
+        changeOrigin: true
+      }
+    }
+  },
 })
