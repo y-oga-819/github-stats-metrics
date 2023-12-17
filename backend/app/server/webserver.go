87cf75a28802a789a8f0295b5837208216bef211
yuki819 <yuki819@mba2018.local>
2023-12-17 23:00:34 +0900

    feat: [Get] /api/pull_requests のルーティングを設定

--- backend/app/server/webserver.go
+++ backend/app/server/webserver.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gorilla/mux"
 
+	pullRequestUseCase "github-stats-metrics/application/pull_request"
 	todoUseCase "github-stats-metrics/application/todo"
 )
 
@@ -16,6 +17,7 @@ func StartWebServer() error {
 
 	// URL に呼び出したい関数を登録する
 	r.HandleFunc("/api/todos", todoUseCase.GetTodos).Methods("GET")
+	r.HandleFunc("/api/pull_requests", pullRequestUseCase.GetPullRequests).Methods("GET")
 
 	// ポートを指定してサーバーを起動する
 	return http.ListenAndServe(":8080", r)
