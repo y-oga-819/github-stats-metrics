a2023971ddca98ab13ce6f8b3b594d625780d38b
y-oga-819 <yukineko.mew7@gmail.com>
2024-01-06 21:32:37 +0900

    feat: 仮のスプリント情報を返す関数を実装

---
+++ frontend/src/features/sprintlist/GetConstSprintList.ts
@@ -0,0 +1,175 @@
+import { Sprint } from "./SprintRow";
+
+export const GetSprintList = (): Sprint[] => {
+    const result: Sprint[] = [];
+
+    result.push(
+        {
+            id: 1,
+            startDate: new Date('2023-10-04'),
+            endDate: new Date('2023-10-10'),
+            members: [
+                {name: 'y-oga-819', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+                {name: 'shiiyan', iconURL: 'https://avatars.githubusercontent.com/u/36617009?s=72&u=7bc425214658fa06bf6f1d254ff6d22c0accc309&v=4'},
+                {name: 'yamaoka-chiwawa', iconURL: 'https://avatars.githubusercontent.com/u/20041029?s=72&u=bef2ea9d5fb6af3532f17ee08743493f7b6be9a5&v=4'},
+                {name: 'toshi-oliver', iconURL: 'https://avatars.githubusercontent.com/u/39400536?s=72&u=27798bba0d7d59c648b65170e51ccb32852ab040&v=4'},
+                {name: 'EtsuhisaNakamura', iconURL: 'https://avatars.githubusercontent.com/u/53359731?s=72&u=244e45dee16d710fa4e71d741de74fc84a4bc96c&v=4'},
+                {name: 'okamotoshuhei-binc', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+            ]
+        },
+        {
+            id: 2,
+            startDate: new Date('2023-10-11'),
+            endDate: new Date('2023-10-17'),
+            members: [
+                {name: 'y-oga-819', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+                {name: 'shiiyan', iconURL: 'https://avatars.githubusercontent.com/u/36617009?s=72&u=7bc425214658fa06bf6f1d254ff6d22c0accc309&v=4'},
+                {name: 'yamaoka-chiwawa', iconURL: 'https://avatars.githubusercontent.com/u/20041029?s=72&u=bef2ea9d5fb6af3532f17ee08743493f7b6be9a5&v=4'},
+                {name: 'toshi-oliver', iconURL: 'https://avatars.githubusercontent.com/u/39400536?s=72&u=27798bba0d7d59c648b65170e51ccb32852ab040&v=4'},
+                {name: 'EtsuhisaNakamura', iconURL: 'https://avatars.githubusercontent.com/u/53359731?s=72&u=244e45dee16d710fa4e71d741de74fc84a4bc96c&v=4'},
+                {name: 'okamotoshuhei-binc', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+            ]
+        },
+        {
+            id: 3,
+            startDate: new Date('2023-10-18'),
+            endDate: new Date('2023-10-24'),
+            members: [
+                {name: 'y-oga-819', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+                {name: 'shiiyan', iconURL: 'https://avatars.githubusercontent.com/u/36617009?s=72&u=7bc425214658fa06bf6f1d254ff6d22c0accc309&v=4'},
+                {name: 'yamaoka-chiwawa', iconURL: 'https://avatars.githubusercontent.com/u/20041029?s=72&u=bef2ea9d5fb6af3532f17ee08743493f7b6be9a5&v=4'},
+                {name: 'toshi-oliver', iconURL: 'https://avatars.githubusercontent.com/u/39400536?s=72&u=27798bba0d7d59c648b65170e51ccb32852ab040&v=4'},
+                {name: 'EtsuhisaNakamura', iconURL: 'https://avatars.githubusercontent.com/u/53359731?s=72&u=244e45dee16d710fa4e71d741de74fc84a4bc96c&v=4'},
+                {name: 'okamotoshuhei-binc', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+            ]
+        },
+        {
+            id: 4,
+            startDate: new Date('2023-10-25'),
+            endDate: new Date('2023-10-31'),
+            members: [
+                {name: 'y-oga-819', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+                {name: 'shiiyan', iconURL: 'https://avatars.githubusercontent.com/u/36617009?s=72&u=7bc425214658fa06bf6f1d254ff6d22c0accc309&v=4'},
+                {name: 'yamaoka-chiwawa', iconURL: 'https://avatars.githubusercontent.com/u/20041029?s=72&u=bef2ea9d5fb6af3532f17ee08743493f7b6be9a5&v=4'},
+                {name: 'toshi-oliver', iconURL: 'https://avatars.githubusercontent.com/u/39400536?s=72&u=27798bba0d7d59c648b65170e51ccb32852ab040&v=4'},
+                {name: 'EtsuhisaNakamura', iconURL: 'https://avatars.githubusercontent.com/u/53359731?s=72&u=244e45dee16d710fa4e71d741de74fc84a4bc96c&v=4'},
+                {name: 'okamotoshuhei-binc', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+            ]
+        },
+        {
+            id: 5,
+            startDate: new Date('2023-11-01'),
+            endDate: new Date('2023-11-07'),
+            members: [
+                {name: 'y-oga-819', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+                {name: 'shiiyan', iconURL: 'https://avatars.githubusercontent.com/u/36617009?s=72&u=7bc425214658fa06bf6f1d254ff6d22c0accc309&v=4'},
+                {name: 'yamaoka-chiwawa', iconURL: 'https://avatars.githubusercontent.com/u/20041029?s=72&u=bef2ea9d5fb6af3532f17ee08743493f7b6be9a5&v=4'},
+                {name: 'toshi-oliver', iconURL: 'https://avatars.githubusercontent.com/u/39400536?s=72&u=27798bba0d7d59c648b65170e51ccb32852ab040&v=4'},
+                {name: 'EtsuhisaNakamura', iconURL: 'https://avatars.githubusercontent.com/u/53359731?s=72&u=244e45dee16d710fa4e71d741de74fc84a4bc96c&v=4'},
+                {name: 'okamotoshuhei-binc', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+            ]
+        },
+        {
+            id: 6,
+            startDate: new Date('2023-11-08'),
+            endDate: new Date('2023-11-14'),
+            members: [
+                {name: 'y-oga-819', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+                {name: 'shiiyan', iconURL: 'https://avatars.githubusercontent.com/u/36617009?s=72&u=7bc425214658fa06bf6f1d254ff6d22c0accc309&v=4'},
+                {name: 'yamaoka-chiwawa', iconURL: 'https://avatars.githubusercontent.com/u/20041029?s=72&u=bef2ea9d5fb6af3532f17ee08743493f7b6be9a5&v=4'},
+                {name: 'toshi-oliver', iconURL: 'https://avatars.githubusercontent.com/u/39400536?s=72&u=27798bba0d7d59c648b65170e51ccb32852ab040&v=4'},
+                {name: 'EtsuhisaNakamura', iconURL: 'https://avatars.githubusercontent.com/u/53359731?s=72&u=244e45dee16d710fa4e71d741de74fc84a4bc96c&v=4'},
+                {name: 'okamotoshuhei-binc', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+            ]
+        },
+        {
+            id: 7,
+            startDate: new Date('2023-11-15'),
+            endDate: new Date('2023-11-21'),
+            members: [
+                {name: 'y-oga-819', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+                {name: 'shiiyan', iconURL: 'https://avatars.githubusercontent.com/u/36617009?s=72&u=7bc425214658fa06bf6f1d254ff6d22c0accc309&v=4'},
+                {name: 'yamaoka-chiwawa', iconURL: 'https://avatars.githubusercontent.com/u/20041029?s=72&u=bef2ea9d5fb6af3532f17ee08743493f7b6be9a5&v=4'},
+                {name: 'toshi-oliver', iconURL: 'https://avatars.githubusercontent.com/u/39400536?s=72&u=27798bba0d7d59c648b65170e51ccb32852ab040&v=4'},
+                {name: 'EtsuhisaNakamura', iconURL: 'https://avatars.githubusercontent.com/u/53359731?s=72&u=244e45dee16d710fa4e71d741de74fc84a4bc96c&v=4'},
+                {name: 'okamotoshuhei-binc', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+            ]
+        },
+        {
+            id: 8,
+            startDate: new Date('2023-11-22'),
+            endDate: new Date('2023-11-28'),
+            members: [
+                {name: 'y-oga-819', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+                {name: 'shiiyan', iconURL: 'https://avatars.githubusercontent.com/u/36617009?s=72&u=7bc425214658fa06bf6f1d254ff6d22c0accc309&v=4'},
+                {name: 'yamaoka-chiwawa', iconURL: 'https://avatars.githubusercontent.com/u/20041029?s=72&u=bef2ea9d5fb6af3532f17ee08743493f7b6be9a5&v=4'},
+                {name: 'toshi-oliver', iconURL: 'https://avatars.githubusercontent.com/u/39400536?s=72&u=27798bba0d7d59c648b65170e51ccb32852ab040&v=4'},
+                {name: 'EtsuhisaNakamura', iconURL: 'https://avatars.githubusercontent.com/u/53359731?s=72&u=244e45dee16d710fa4e71d741de74fc84a4bc96c&v=4'},
+                {name: 'okamotoshuhei-binc', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+            ]
+        },
+        {
+            id: 9,
+            startDate: new Date('2023-11-29'),
+            endDate: new Date('2023-12-05'),
+            members: [
+                {name: 'y-oga-819', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+                {name: 'shiiyan', iconURL: 'https://avatars.githubusercontent.com/u/36617009?s=72&u=7bc425214658fa06bf6f1d254ff6d22c0accc309&v=4'},
+                {name: 'yamaoka-chiwawa', iconURL: 'https://avatars.githubusercontent.com/u/20041029?s=72&u=bef2ea9d5fb6af3532f17ee08743493f7b6be9a5&v=4'},
+                {name: 'toshi-oliver', iconURL: 'https://avatars.githubusercontent.com/u/39400536?s=72&u=27798bba0d7d59c648b65170e51ccb32852ab040&v=4'},
+                {name: 'EtsuhisaNakamura', iconURL: 'https://avatars.githubusercontent.com/u/53359731?s=72&u=244e45dee16d710fa4e71d741de74fc84a4bc96c&v=4'},
+                {name: 'okamotoshuhei-binc', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+            ]
+        },
+        {
+            id: 10,
+            startDate: new Date('2023-12-06'),
+            endDate: new Date('2023-12-12'),
+            members: [
+                {name: 'y-oga-819', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+                {name: 'shiiyan', iconURL: 'https://avatars.githubusercontent.com/u/36617009?s=72&u=7bc425214658fa06bf6f1d254ff6d22c0accc309&v=4'},
+                {name: 'yamaoka-chiwawa', iconURL: 'https://avatars.githubusercontent.com/u/20041029?s=72&u=bef2ea9d5fb6af3532f17ee08743493f7b6be9a5&v=4'},
+                {name: 'toshi-oliver', iconURL: 'https://avatars.githubusercontent.com/u/39400536?s=72&u=27798bba0d7d59c648b65170e51ccb32852ab040&v=4'},
+                {name: 'EtsuhisaNakamura', iconURL: 'https://avatars.githubusercontent.com/u/53359731?s=72&u=244e45dee16d710fa4e71d741de74fc84a4bc96c&v=4'},
+                {name: 'okamotoshuhei-binc', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+            ]
+        },
+        {
+            id: 11,
+            startDate: new Date('2023-12-13'),
+            endDate: new Date('2023-12-19'),
+            members: [
+                {name: 'y-oga-819', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+                {name: 'shiiyan', iconURL: 'https://avatars.githubusercontent.com/u/36617009?s=72&u=7bc425214658fa06bf6f1d254ff6d22c0accc309&v=4'},
+                {name: 'yamaoka-chiwawa', iconURL: 'https://avatars.githubusercontent.com/u/20041029?s=72&u=bef2ea9d5fb6af3532f17ee08743493f7b6be9a5&v=4'},
+                {name: 'toshi-oliver', iconURL: 'https://avatars.githubusercontent.com/u/39400536?s=72&u=27798bba0d7d59c648b65170e51ccb32852ab040&v=4'},
+                {name: 'EtsuhisaNakamura', iconURL: 'https://avatars.githubusercontent.com/u/53359731?s=72&u=244e45dee16d710fa4e71d741de74fc84a4bc96c&v=4'},
+            ]
+        },
+        {
+            id: 12,
+            startDate: new Date('2023-12-20'),
+            endDate: new Date('2023-12-26'),
+            members: [
+                {name: 'y-oga-819', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+                {name: 'shiiyan', iconURL: 'https://avatars.githubusercontent.com/u/36617009?s=72&u=7bc425214658fa06bf6f1d254ff6d22c0accc309&v=4'},
+                {name: 'yamaoka-chiwawa', iconURL: 'https://avatars.githubusercontent.com/u/20041029?s=72&u=bef2ea9d5fb6af3532f17ee08743493f7b6be9a5&v=4'},
+                {name: 'toshi-oliver', iconURL: 'https://avatars.githubusercontent.com/u/39400536?s=72&u=27798bba0d7d59c648b65170e51ccb32852ab040&v=4'},
+                {name: 'EtsuhisaNakamura', iconURL: 'https://avatars.githubusercontent.com/u/53359731?s=72&u=244e45dee16d710fa4e71d741de74fc84a4bc96c&v=4'},
+            ]
+        },
+        {
+            id: 13,
+            startDate: new Date('2023-12-27'),
+            endDate: new Date('2024-01-09'),
+            members: [
+                {name: 'y-oga-819', iconURL: 'https://avatars.githubusercontent.com/u/6323203?s=72&u=666f35cd6b55e42ad016eca28262b5a4e49aa4a6&v=4'},
+                {name: 'shiiyan', iconURL: 'https://avatars.githubusercontent.com/u/36617009?s=72&u=7bc425214658fa06bf6f1d254ff6d22c0accc309&v=4'},
+                {name: 'yamaoka-chiwawa', iconURL: 'https://avatars.githubusercontent.com/u/20041029?s=72&u=bef2ea9d5fb6af3532f17ee08743493f7b6be9a5&v=4'},
+                {name: 'toshi-oliver', iconURL: 'https://avatars.githubusercontent.com/u/39400536?s=72&u=27798bba0d7d59c648b65170e51ccb32852ab040&v=4'},
+                {name: 'EtsuhisaNakamura', iconURL: 'https://avatars.githubusercontent.com/u/53359731?s=72&u=244e45dee16d710fa4e71d741de74fc84a4bc96c&v=4'},
+            ]
+        },
+    )
+    return result;
+}
