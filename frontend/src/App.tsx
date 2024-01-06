70089a969f84ba07169fbb2777bdbdcae37c2265
y-oga-819 <yukineko.mew7@gmail.com>
2024-01-06 21:34:30 +0900

    feat: PR一覧はスプリント詳細に表示するのでトップページから消す

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,12 @@
 import './App.css'
-import { PullRequestList } from './features/pullrequestlist/PullRequestList';
 import { BrowserRouter } from 'react-router-dom';
 import { SprintList } from './features/sprintlist/SprintList';
 
 export const App = () => {
   return (
     <>
-      {/* <Chart prs={pullRequests}/> */}
       <BrowserRouter>
         <SprintList/>
-        <PullRequestList/>
       </BrowserRouter>
     </>
   )
