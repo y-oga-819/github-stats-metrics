acba7614551b6998dae26fe1dce80cc2bc747a14
y-oga-819 <yukineko.mew7@gmail.com>
2024-01-08 23:07:32 +0900

    feat: メトリクスを表示できるようにする

--- frontend/src/features/Chart/Chart.tsx
+++ frontend/src/features/Chart/Chart.tsx
@@ -1,68 +1,128 @@
-export const Chart = () => {
-  // const prIds = prs?.map((pr: PR) => {return pr.id})
-  //  const betweenFirstReviewAndCreated = prs?.map((pr: PR) => {
-  //   return pr.firstReviewed.getTime() - pr.created.getTime()
-  // }) ?? []
-  // const betweenLastApprovedAndFirstReviewed = prs?.map((pr: PR) => {
-  //   return pr.lastApproved.getTime() - pr.firstReviewed.getTime()
-  // }) ?? []
-  // const betweenMergedAndLastApproved = prs?.map((pr: PR) => {
-  //   return pr.merged.getTime() - pr.lastApproved.getTime()
-  // }) ?? []
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { GetSprintList } from "../sprintlist/GetConstSprintList";
+import { useEffect, useState } from "react";
+import { FetchPullRequests } from "../pullrequestlist/PullRequestsFetcher";
 
-  // const chartData: ApexOptions = {
-  //     chart: {
-  //       type: "line",
-  //       id: "apexchart-example",
-  //       stacked: true,
-  //     },
-  //     xaxis: {
-  //       categories: prIds
-  //     },
-  //     fill: {
-  //       type: "gradient",
-  //       gradient: {
-  //         shade: "light",
-  //         type: "horizontal",
-  //         shadeIntensity: 0.5,
-  //         gradientToColors: undefined, // optional, if not defined - uses the shades of same color in series
-  //         inverseColors: true,
-  //         opacityFrom: 1,
-  //         opacityTo: 1,
-  //         stops: [0, 50, 100]
-  //       }
-  //     },
-  //     legend: {
-  //       // position: '',
-  //       width: 300
-  //       // position: 'top',
-  //     },
-  //     series: [
-  //       {
-  //         name: "betweenFirstReviewAndCreated",
-  //         type: "column",
-  //         data: betweenFirstReviewAndCreated,
-  //       },
-  //       {
-  //           name: "betweenLastApprovedAndFirstReviewed",
-  //           type: "column",
-  //           data: betweenLastApprovedAndFirstReviewed,
-  //       },
-  //       {
-  //         name: "betweenMergedAndLastApproved",
-  //         type: "column",
-  //         data: betweenMergedAndLastApproved,
-  //       }, 
-  //       {
-  //         name: "Time Traveled",
-  //         type: "line",
-  //         data: [23, 42, 35, 27, 43, 22, 17, 31, 42, 22, 12, 16],
-  //       }
-  //     ]
-  //   };
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const chartOptions = {
+  plugins: {
+    title: {
+      display: true,
+      text: 'スプリントあたりの開発メトリクスチャート',
+    },
+    annotation: { 
+      annotations: [{ 
+        type: 'line', // 線を描画
+        id: '2dayBorder', 
+        mode: 'horizontal', // 線を水平に引く
+        scaleID: 'y-axis-0', 
+        value: 86400, // 基準となる数値
+        borderWidth: 10, // 基準線の太さ
+        borderColor: 'red'  // 基準線の色
+      }] 
+    },  
+  },
+  responsive: true,
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true,
+    },
+  },
+}
+
+type Metrics = {
+  sprintId: number,
+  score: number,
+}
+
+export const Chart = () => {
+  const sprintList = GetSprintList()
+  const labels = sprintList.map((sprint) => sprint.id)
+
+  const [untilFirstReviewedList, setUntilFirstReviewedList] = useState<Metrics[]>([]);
+  const [untilLastApprovedList, setUntilLastApprovedList] = useState<Metrics[]>([]);
+  const [untilMergedList, setUntilMergedList] = useState<Metrics[]>([]);
+
+  const datasets = {
+    labels,
+    datasets: [
+      {
+        label: 'レビューまでにかかった時間',
+        data: untilFirstReviewedList.map((metrics) => metrics.score),
+        backgroundColor: 'rgb(255, 99, 132)',
+      },
+      {
+        label: '最後のapproveまでにかかった時間',
+        data: untilLastApprovedList.map((metrics) => metrics.score),
+        backgroundColor: 'rgb(75, 192, 192)',
+      },
+      {
+        label: 'マージまでにかかった時間',
+        data: untilMergedList.map((metrics) => metrics.score),
+        backgroundColor: 'rgb(53, 162, 235)',
+      },
+    ],
+  };
+
+  useEffect(() => {
+    // スプリント毎のPRをチャートに反映する
+    sprintList.map((sprint) => {
+      // 1スプリント分のPRを取得
+      const prs = FetchPullRequests(sprint)
+      prs.then((prs) => prs.filter((pr) => !pr.branchName.startsWith("epic/"))) // epicブランチは除外する
+      .then((prs) => {
+        // PR数を計算
+        const prCount = prs.length
+
+        // レビューまでにかかった時間を計算
+        const untilFirstReviewed = prs
+          .map((pr) => (pr.firstReviewed.getTime() - pr.created.getTime()) / 1000)
+          .reduce((a, b) => a + b, 0) / prCount;
+
+        // 最後のapproveまでにかかった時間を計算
+        const untilLastApproved = prs
+          .map((pr) => (pr.lastApproved.getTime() - pr.firstReviewed.getTime()) / 1000)
+          .reduce((a, b) => a + b, 0) / prCount  
   
-    return (
-        // <ReactApexChart options={chartData} series={chartData.series} />
-        <h1>Chart Page</h1>
-    );
+        // マージまでにかかった時間を計算
+        const untilMerged = prs
+          .map((pr) => (pr.merged.getTime() - pr.lastApproved.getTime()) / 1000)
+          .reduce((a, b) => a + b, 0) / prCount
+
+        // これらの値をチャート用のデータに追加する
+        setUntilFirstReviewedList((untilFirstReviewedList) => [...untilFirstReviewedList, {sprintId: sprint.id, score: untilFirstReviewed}])
+        setUntilLastApprovedList((untilLastApprovedList) => [...untilLastApprovedList, {sprintId: sprint.id, score: untilLastApproved}])
+        setUntilMergedList((untilMergedList) => [...untilMergedList, {sprintId: sprint.id, score: untilMerged}])
+      })
+    })
+}, []);
+
+return (
+    <>
+      {datasets
+       ? <Bar options={chartOptions} data={datasets} />
+       : <div>Loading...</div>
+      }
+    </>
+  )
 };
